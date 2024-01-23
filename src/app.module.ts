13da0892503be7386edad1959829094db6e19407
Luis Carlos <lucarupa65@gmail.com>
2024-01-22 21:29:29 -0500

    Conexion a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault  } from '@apollo/server/plugin/landingPage/default';
@@ -10,6 +12,7 @@ import { TickestModule } from './tickest/tickest.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
@@ -18,6 +21,17 @@ import { TickestModule } from './tickest/tickest.module';
       plugins: [
         ApolloServerPluginLandingPageLocalDefault()
       ]
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.BD_USERNAME,
+      password: process.env.BD_PASSWORD,
+      database: process.env.BD_NAME,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true
     }),
     TickestModule,
   ],
